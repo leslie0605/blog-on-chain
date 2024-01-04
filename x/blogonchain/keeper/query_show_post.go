5f4e8f669350615e4a63d9636a2870f2f7110290
leslie0605 <737888086@qq.com>
2024-01-04 22:19:19 +0800

    ignite scaffold message and query implementation

--- x/blogonchain/keeper/query_show_post.go
+++ x/blogonchain/keeper/query_show_post.go
@@ -3,21 +3,24 @@ package keeper
 import (
 	"context"
 
-	"blog-on-chain/x/blogonchain/types"
+	"blog/x/blog/types"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
 func (k Keeper) ShowPost(goCtx context.Context, req *types.QueryShowPostRequest) (*types.QueryShowPostResponse, error) {
-	if req == nil {
-		return nil, status.Error(codes.InvalidArgument, "invalid request")
-	}
+    if req == nil {
+        return nil, status.Error(codes.InvalidArgument, "invalid request")
+    }
 
-	ctx := sdk.UnwrapSDKContext(goCtx)
+    ctx := sdk.UnwrapSDKContext(goCtx)
+    post, found := k.GetPost(ctx, req.Id)
+    if !found {
+        return nil, sdkerrors.ErrKeyNotFound
+    }
 
-	// TODO: Process the query
-	_ = ctx
-
-	return &types.QueryShowPostResponse{}, nil
+    return &types.QueryShowPostResponse{Post: &post}, nil
 }
--- x/blogonchain/types/keys.go
+++ x/blogonchain/types/keys.go
@@ -9,6 +9,8 @@ const (
 
 	// MemStoreKey defines the in-memory store key
 	MemStoreKey = "mem_blogonchain"
+	PostKey = "Post/value/"
+	PostCountKey = "Post/count/"
 )
 
 var (
